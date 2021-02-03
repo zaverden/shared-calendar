9b823a04b12bd27c2c098cc39ad94b74014596d2
zaverden <zaverden@gmail.com>
2021-02-03 14:18:29 +0700

    setup build script

---
+++ build.js
@@ -0,0 +1,21 @@
+const glob = require("tiny-glob");
+const {
+  promises: { unlink },
+} = require("fs");
+
+const [, , arg] = process.argv;
+
+(async () => {
+  const jsPaths = await glob("src/**/*.{js,js.map}");
+  Promise.all(jsPaths.map((p) => unlink(p)));
+  const tsPaths = await glob("src/**/*.ts");
+  await require("esbuild").build({
+    entryPoints: tsPaths,
+    outdir: "src",
+    platform: "node",
+    target: "node10.9.0",
+    sourcemap: true,
+    sourcesContent: true,
+    watch: arg === "--watch",
+  });
+})();
