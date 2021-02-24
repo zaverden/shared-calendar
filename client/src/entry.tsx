dc96b94e745baed0bdc7f275095f2d74f05d25ec
zaverden <zaverden@gmail.com>
2021-02-24 12:47:47 +0700

    add first client page

--- client/src/entry.tsx
+++ client/src/entry.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
-const Greet = () => <h1>Hello, there!</h1>;
+const Greet = () => (
+  <Fragment>
+    <h1>Hello, there!</h1>
+    <a href="/auth/google">Login with google</a>
+  </Fragment>
+);
 ReactDOM.render(
   <React.StrictMode>
     <Greet />
