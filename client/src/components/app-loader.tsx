6d3aab02c2261d0a8b422ee4b3d03d761f39abf0
zaverden <zaverden@gmail.com>
2021-03-08 22:50:51 +0700

    update app loader

--- client/src/components/app-loader.tsx
+++ client/src/components/app-loader.tsx
@@ -1,5 +1,54 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
 import React from "react";
 
+const RippleAnimation = keyframes`
+  0% {
+    width: 0;
+    height: 0;
+    margin-top: 50%;
+    margin-left: 50%;
+    opacity: 1;
+  }
+  100% {
+    width: 100%;
+    height: 100%;
+    margin-top: 0;
+    margin-left: 0;
+    opacity: 0;
+  }
+`;
+
+const Loader = styled.div`
+  width: 100%;
+  padding-bottom: 100%;
+  position: relative;
+
+  & div {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    border: 4px solid var(--bg-p);
+    opacity: 1;
+    border-radius: 50%;
+    animation: ${RippleAnimation} 2.4s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+
+  & div:nth-child(2) {
+    animation-delay: -0.8s;
+  }
+
+  & div:nth-child(3) {
+    animation-delay: -1.6s;
+  }
+`;
+
 export function AppLoader() {
-  return <div>LOADING...</div>;
+  return (
+    <Loader>
+      <div />
+      <div />
+      <div />
+    </Loader>
+  );
 }
